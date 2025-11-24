d30898f20792ce40edfedce9329eed328f3b4d9b
corridda <40513806+corridda@users.noreply.github.com>
2025-11-24 18:27:28 +0300

    HW5 - types, structs, methods (#2)
    * HW5 - types, structs, methods

---
+++ Basic/Homework/05_structs_methods/internal/model/task.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	Created   = "Создана"
+	Seen      = "Просмотрена"
+	InProcess = "В работе"
+	Suspended = "Приостановлена"
+	Submitted = "Решена, ждет контроля"
+	Completed = "Завершена"
+	Cancelled = "Отменена"
+	Returned  = "Возвращена на доработку"
+	Backlog   = "Бэклог"
+)
+
+type Task struct {
+	name          string
+	description   string
+	initTimeStamp time.Time
+	dueDate       time.Time
+	status        string
+}
+
+func NewTask(name, descr string, dueDate string) Task {
+	userDueDate, err := time.Parse("02.01.2006", dueDate)
+	if err != nil {
+		panic("Введенная дата исполнения имеет не корректный формат.")
+	}
+	myTask := Task{
+		name:          name,
+		description:   descr,
+		initTimeStamp: time.Now(),
+		dueDate:       userDueDate,
+		status:        Created,
+	}
+	return myTask
+}
+
+func (myTask *Task) String() string {
+	return fmt.Sprintf(
+		"Имя задачи: %v\nОписание задачи: %v\nДата постановки задачи: %v\nДата исполнения: %v\nСтатус: %v\n",
+		myTask.name, myTask.description, myTask.initTimeStamp.Format(time.DateTime), myTask.dueDate.Format(time.DateTime), myTask.status,
+	)
+}
