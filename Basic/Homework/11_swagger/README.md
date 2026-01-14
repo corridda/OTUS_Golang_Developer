21dc85877c3e2c5c1562d92165f7aa6d65238819
corridda <40513806+corridda@users.noreply.github.com>
2026-01-14 08:22:45 +0300

    11_swagger initialization

---
+++ Basic/Homework/11_swagger/README.md
@@ -0,0 +1,12 @@
+swag
+Swag converts Go annotations to Swagger Documentation 2.0.
+https://github.com/swaggo/swag
+
+Install swag by using
+`go install github.com/swaggo/swag/cmd/swag@latest`
+
+Как сгенерировать документацию 
+`swag init -g cmd/main.go`
+
+Как перейти в сваггер:
+При запущенном сервере перейти  `http://localhost:8080/swagger/index.html`
---
+++ Basic/Homework/11_swagger/internal/model/note.go
@@ -0,0 +1,47 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+type Note struct {
+	Id             int       `json:"id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description"`
+	AlarmTimeStamp time.Time `json:"alarmTimeStamp"` // Сигнал напоминания в эту дату-время
+}
+
+func NewNote(name, descr, alarmDateTime string) Note {
+	userDueDate, err := time.Parse("02.01.2006 15:04", alarmDateTime)
+	if err != nil {
+		fmt.Println("Введенные дата и время напоминания имеют не корректный формат.")
+		return Note{
+			Name:        name,
+			Description: descr,
+		}
+	} else {
+		return Note{
+			Name:           name,
+			Description:    descr,
+			AlarmTimeStamp: userDueDate,
+		}
+	}
+}
+
+func (myNote Note) String() string {
+	return fmt.Sprintf(
+		"Id заметки: %v\nИмя заметки: %v\nОписание заметки: %v\nДата и время напоминания: %v\n",
+		myNote.Id, myNote.Name, myNote.Description, myNote.AlarmTimeStamp.Format("02.01.2006 15:04"),
+	)
+}
+
+// ChangeAlarm implements repository.Remindable.
+func (myNote *Note) ChangeAlarm(new_date_time string) {
+	userDateTime, err := time.Parse("02.01.2006 15:04", new_date_time)
+	if err != nil {
+		fmt.Println("Введенные дата и время напоминания имеют не корректный формат.\nВведите требуемые значения даты и времени в соответствии с форматом: ДД-ММ-ГГГГ ЧЧ:ММ.")
+	} else {
+		myNote.AlarmTimeStamp = userDateTime
+	}
+}
