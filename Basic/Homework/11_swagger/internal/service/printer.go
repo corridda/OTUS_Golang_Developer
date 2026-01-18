88374775fc45f399653bf874817b6f56a2a32fa7
corridda <40513806+corridda@users.noreply.github.com>
2026-01-18 15:11:54 +0300

    11_swagger initialization (#8)

---
+++ Basic/Homework/11_swagger/internal/service/printer.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/corridda/OTUS_Golang_Developer/Basic/Homework/11_swagger/internal/repository"
+)
+
+func PrintRemidables(mutex *sync.RWMutex) {
+	mutex.RLock()
+	fmt.Println("Имеющиеся задачи:")
+	for _, t := range repository.Tasks {
+		fmt.Println(t.String())
+	}
+	mutex.RUnlock()
+
+	mutex.RLock()
+	fmt.Println("\nИмеющиеся заметки:")
+	for _, n := range repository.Notes {
+		fmt.Println(n.String())
+	}
+	mutex.RUnlock()
+}
